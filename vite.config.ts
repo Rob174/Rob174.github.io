e1305e66b37d25fc7237f60e284b196abee3eefb
Rob174 <robin.128.456@gmail.com>
2023-05-14 23:57:23 -0400

    update index

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "./docs", // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-  },
+  plugins: [svelte()]
 });
